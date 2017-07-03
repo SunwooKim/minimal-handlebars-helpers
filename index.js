1b7a1fcd3ef9f349efd52582a067e4e38af84d7e
Sunwoo Kim <s.kim@navercorp.com>
2017-07-03 14:32:56 +0900

    replace moment helper

--- index.js
+++ index.js
@@ -25,6 +25,43 @@ helpers.register = function (Handlebars, namespaces) {
       });
     }
   });
+
+  var moment  = require('moment-timezone');
+  var _       = require('lodash');
+
+  Handlebars.registerHelper('moment', function (context, block) {
+    if (context && context.hash) {
+      block = _.cloneDeep(context);
+      context = undefined;
+    }
+    var date = moment(context);
+    
+    if (block.hash.timezone){
+      date.tz(block.hash.timezone);
+    }
+
+    var hasFormat = false;
+
+    // Reset the language back to default before doing anything else
+    date.lang('en');
+
+    for (var i in block.hash) {
+      if (i === 'format') {
+        hasFormat = true;
+      }
+      else if (date[i]) {
+        date = date[i](block.hash[i]);
+      } else {
+        console.log('moment.js does not support "' + i + '"');
+      }
+    }
+
+    if (hasFormat) {
+      date = date.format(block.hash.format);
+    }
+    return date;
+  });
+
 };
 
 module.exports = helpers;
